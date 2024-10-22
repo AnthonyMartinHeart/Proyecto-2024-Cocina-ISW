85a6ea63fa37e0c09eb8e79ebb9c2cd9369c8c20
AnthonyMartinHeart <138081463+AnthonyMartinHeart@users.noreply.github.com>
2024-10-21 22:53:34 -0300

    Update index.routes.js

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -3,12 +3,14 @@ import { Router } from "express";
 import userRoutes from "./user.routes.js";
 import authRoutes from "./auth.routes.js";
 import empleadoRoutes from "./empleado.routes.js"; // Importar las rutas de empleados
+import orderRoutes from "./order.routes.js"; // Importar las rutas de pedidos
 
 const router = Router();
 
 router
     .use("/auth", authRoutes)
     .use("/user", userRoutes)
-    .use("/empleados", empleadoRoutes); // Rutas de empleados
+    .use("/empleados", empleadoRoutes) // Rutas de empleados
+    .use("/orders", orderRoutes); // Rutas de pedidos
 
 export default router;
